514f83b781cd13de5ea9eba18bb73a8635474468
jmpirola <jmpirola@gmail.com>
2015-11-02 23:31:14 -0300

    InitialLoad
    InitialLoad

---
+++ FirstMVCProject/Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstMVCProject.Models;
+
+namespace FirstMVCProject.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ViewResult DisplayCustomer(Customer objCustomer )
+        {
+            /*
+                        Customer objCustomer = new Customer();
+
+                       objCustomer.Id = Convert.ToInt16(Request.Form["Id"].ToString());
+                       objCustomer.CustomerCode = Request.Form["CustomerCode"].ToString();
+                       objCustomer.Amount = Convert.ToDouble(Request.Form["Amount"].ToString()); ;
+            */
+            return View("DisplayCustomerView", objCustomer);
+        }
+
+        public ViewResult FillCustomer()
+        {
+            return View("FillCustomerView");
+        }
+    }
+}
---
+++ FirstMVCProject/Controllers/DisplayTimeController.cs
@@ -0,0 +1,18 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVCProject.Controllers
+{
+    public class DisplayTimeController : Controller
+    {
+        // GET: DisplayTime
+        public ActionResult Index()
+        {
+            ViewData["CurrentTime"] = DateTime.Now.ToString();
+            return View("DisplayTimeView");
+        }
+    }
+}
